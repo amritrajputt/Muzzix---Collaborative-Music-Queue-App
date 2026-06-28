f0b061f72dcf2a163fbcf364af155ec57c247e3f
amrit <vishal179singh@gmail.com>
2026-06-28 11:06:42 +0530

    added idempotency key in header of request

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
   }
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, x-guest-uuid");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, x-guest-uuid, x-idempotency-key");
   res.setHeader("Access-Control-Allow-Credentials", "true");
   
   if (req.method === "OPTIONS") {
